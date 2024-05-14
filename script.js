fc4af2a1faaf6c2defda1f2d18947a20749b42c7
Chidindu Emmanuel <chidinduemma123@gmail.com>
2024-05-14 12:25:08 +0100

    initial commit..

---
+++ script.js
@@ -0,0 +1,19 @@
+function redGreenBlueCount(arr) {
+  var RED = 0, GREEN = 1, BLUE = 2;
+  var counter = new Array(3).fill(0);
+
+  for (var i = 0; i < arr.length; i++) {
+    var curr = arr[i];
+    if (curr == RED) {
+      counter[RED]++;
+    } else if (curr == GREEN) {
+      counter[GREEN]++;
+    } else if (curr == BLUE) {
+      counter[BLUE]++;
+    }
+  }
+
+  return counter;
+}
+
+redGreenBlueCount([0, 1, 1, 1, 2, 2, 2]); // [1, 3, 3]
